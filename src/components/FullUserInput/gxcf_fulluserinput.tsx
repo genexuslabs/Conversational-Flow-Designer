eda9c7ff44fd5a61b2dce1f02ec827bcc3a98879
Diego Scaffo <dscaffo@genexus.com>
2019-10-31 15:25:57 -0300

    Switch basic/advanced mode - Issue: 74702

--- src/components/FullUserInput/gxcf_fulluserinput.tsx
+++ src/components/FullUserInput/gxcf_fulluserinput.tsx
@@ -10,30 +10,67 @@ import { HintId } from "../../global/ConversationalEditor/helpers/Helpers";
 })
 export class GXCF_FullUserInput {
   @Prop() userInput: UserInputElement;
-
+  @State() enableAdvancedMode: boolean = false;
+  
   @Event() onCollapseUserInput: EventEmitter;
   TriggerOnCollapseUserInput(event){    
     this.onCollapseUserInput.emit(event);
   }
 
+  SwitchAdvancedMode(event){
+    console.log(event);
+    this.enableAdvancedMode = !this.enableAdvancedMode;
+  }
+
+  private RenderBasicMode():any
+  {
+    return (
+      <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Ask messages</span><gxcf-hint hintId={HintId.AskMessages} class="UserInputHints"/></summary>
+        <gxcf-collection collection={this.userInput.RequiredMessages} collectionAddText="Add another ask message"></gxcf-collection>
+      </details>
+    );
+  }
+
+  private RenderAdvancedMode():any
+  {
+    return (
+      <div>
+        <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Condition to be required</span><gxcf-hint hintId={HintId.Required} class="UserInputHints"/></summary>
+          <p>Test</p>
+        </details>
+        {this.RenderBasicMode()}
+        <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Validate User Input</span><gxcf-hint hintId={HintId.ValidateUserInput} class="UserInputHints"/></summary>
+        </details>
+        <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Redirection</span><gxcf-hint hintId={HintId.Redirection} class="UserInputHints"/></summary>
+        </details>
+      </div>
+    );
+  }
 
   render() {
+      let editionMode:any;
+      let advancedEditionStatus:string;
+      let switchClass:string;
+      if (this.enableAdvancedMode)
+      {
+        switchClass = "SwitchIconOn";
+        editionMode = this.RenderAdvancedMode();
+        advancedEditionStatus = "ON";
+      }        
+      else
+      {
+        switchClass = "SwitchIconOff";
+        advancedEditionStatus = "OFF";
+        editionMode = this.RenderBasicMode();
+      }        
+
       return (
         <div class="FullUserInput">
           <input type="text" class="UserInputTitle" value={this.userInput.Variable} />
           <gxcf-uparrow class="ExpandUserInputDownArrow" onClick={ (event) => this.TriggerOnCollapseUserInput(event)}></gxcf-uparrow>
           <p class="DataType">Datatype: {this.userInput.DataType}</p>   
-          <img class="SwitchIconOn"/><span class="TextMode">Advanced mode ON</span>
-          <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Condition to be required</span><gxcf-hint hintId={HintId.Required} class="UserInputHints"/></summary>
-          <p>Test</p>
-          </details>
-          <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Ask messages</span><gxcf-hint hintId={HintId.AskMessages} class="UserInputHints"/></summary>
-            <gxcf-collection collection={this.userInput.RequiredMessages} collectionAddText="Add another ask message"></gxcf-collection>
-          </details>
-          <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Validate User Input</span><gxcf-hint hintId={HintId.ValidateUserInput} class="UserInputHints"/></summary>
-          </details>
-          <details><summary class="UserInputPart"><span class="UserInputPartSummaryText">Redirection</span><gxcf-hint hintId={HintId.Redirection} class="UserInputHints"/></summary>
-          </details>
+          <img class={switchClass} onClick={ (event) => this.SwitchAdvancedMode(event) }/><span class="TextMode">Advanced mode {advancedEditionStatus}</span>
+          { editionMode }
         </div>
       );
   }
