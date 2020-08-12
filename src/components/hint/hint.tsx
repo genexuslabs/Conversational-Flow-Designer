a1c1dd52fded56078bea1e0e12023921867c5d87
Diego Scaffo <dscaffo@genexus.com>
2020-08-12 09:47:58 -0300

    Hints improvements

--- src/components/hint/hint.tsx
+++ src/components/hint/hint.tsx
@@ -1,74 +1,53 @@
-import { Component, Prop, h, State, Element } from "@stencil/core";
+import { Component, Prop, h, Element } from "@stencil/core";
 import { PropertiesDefinition } from "../common/helpers";
 import { Locale } from "../common/locale";
 
 @Component({
   tag: "gxcf-hint",
-  styleUrl: "hint.scss",
   shadow: true,
   assetsDirs: ["assets/gxcf-hint-lang"]
 })
 export class Hint {
   @Prop() hintId: string;
-  @State() shouldShow = false;
   @Element() element: HTMLElement;
   private componentLocale: any;
   private hintDescription: string;
 
-  HandleShowHint(event): void {
-    console.log(event);
-    this.shouldShow = true;
+  handleShowHint(): void {
+    console.log(this.element);
+    const modal = this.element.shadowRoot.querySelector("gxg-modal");
+    modal.setAttribute("visible", "true");
   }
-
-  HandleHideHint(event): void {
-    console.log(event);
-    this.shouldShow = false;
-  }
-
-  private getShow(): HTMLElement {
-    return (
-      <div class="ShowHint">
-        <span class="gxg-title-03">
-          {PropertiesDefinition.GetTitle(this.hintId)}
-        </span>
-        <gxg-icon
-          type="close"
-          size="small"
-          color="onbackground"
-          onClick={event => this.HandleHideHint(event)}
-          class="CloseHint"
-        />
-        <p class="gxg-text">{this.hintDescription}</p>
-        <a class="gxg-link" href={PropertiesDefinition.GetURL(this.hintId)}>
-          {this.componentLocale.more}
-        </a>
-      </div>
-    );
-  }
-
-  private Hint = (
-    <span class="Hint gxg-link" onClick={event => this.HandleShowHint(event)}>
-      {PropertiesDefinition.GetLabel(this.hintId)}
-    </span>
-  );
 
   async componentWillLoad(): Promise<void> {
     this.componentLocale = await Locale.getComponentStrings(this.element);
+    this.hintDescription = await Locale.getHint(this.hintId);
   }
 
-  async componentWillRender(): Promise<void> {
-    if (this.shouldShow)
-      this.hintDescription = await Locale.getHint(this.hintId);
+  hintIcon(): HTMLElement {
+    return (
+      <gxg-icon
+        size="small"
+        type="notice"
+        color="primary"
+        onClick={() => this.handleShowHint()}
+        style={{ verticalAlign: "middle" }}
+      />
+    );
   }
 
   render() {
-    if (this.shouldShow) {
-      return (
-        <div>
-          {this.Hint}
-          {this.getShow()}
-        </div>
-      );
-    } else return <div>{this.Hint}</div>;
+    return [
+      this.hintIcon(),
+      <gxg-modal modalTitle={PropertiesDefinition.GetTitle(this.hintId)}>
+        <gxg-text>{this.hintDescription}</gxg-text>
+        <gxg-text
+          type="text-link"
+          href={PropertiesDefinition.GetURL(this.hintId)}
+        >
+          {this.componentLocale.more}
+        </gxg-text>
+      </gxg-modal>
+    ];
   }
 }
